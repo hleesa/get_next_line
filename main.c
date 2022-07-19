64b57285608b40efe46d2bf4f4c9c0e7c34bf370
Sanghoon Lee <salee2@c5r2s6.42seoul.kr>
2022-07-19 19:48:55 +0900

    gnl init

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "get_next_line.h"
+#include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	char* line;
+	int	check;
+	int	fd;
+
+	printf
+	fd = open("test.txt", O_RDONLY);
+	while ((check = get_next_line(fd, &line)) > 0)
+	{
+		printf("%s\n", line);
+		free(line);
+	}
+	printf("%s\n", line);
+	free(line);
+	return (0);
+}
