16ef11d246835d0f330a8274ba8fd59112e5899b
Sanghoon Lee <salee2@c5r4s6.42seoul.kr>
2022-07-22 18:27:44 +0900

    gnl ..

--- get_next_line.c
+++ get_next_line.c
@@ -16,63 +16,62 @@
 #define BUFFER_SIZE 10
 
 
-int	add_line(char *ret, char *buff, ssize_t read_bytes)
+int	append_line(char *ret, char *buff)
 {
-	char * addr_of_newline = ft_strchr(buff, '\n');
-
-
-	/*
-	 * 파일의 끝 vs 개행 없는 꽉찬 데이터
-	 */
-
-	if (addr_of_newline == 0)
-	{
-		ft_strjoin(ret, ft_substr(buff, 0, len));
-		buff += len;
-	}
-	else
-	{
-		ft_strjoin(ret, ft_substr(buff, 0, addr_of_newline - buff));
-		buff += addr_of_newline - buff;
-	}
-	return (1);
+	ret = ft_strjoin(ret, buff);
+	return (ret =! NULL);
 }
+
+int append_last_line(char *ret, char *buff)
+{
+	char *addr_of_newline;
+	char *end_of_line;
+
+	addr_of_newline = ft_strchr(buff, '\n');
+	end_of_line = ft_substr(buff, 0, addr_of_newline - buff);
+	ft_strjoin(ret, end_of_line);
+	free(end_of_line);
+	// 위 함수들 반환값 확인 및 에러 호출
+	return (1)
+}
+
+int is_contain_newline(char * buff)
+{
+	char *addr_of_newline;
+
+	addr_of_newline = ft_strchr(buff, '\n');
+	return (addr_of_newline != NULL);
+}
+
 
 char	*get_next_line(int fd)
 {
-	char		**ret;
+	char		*ret;
  	static char	*buff;
 	ssize_t		read_bytes = 0;
+	int 		append_result;
 
-
-	buff = (char *) malloc(sizeof(char) * BUFFER_SIZE);
+	buff = (char *) malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (buff == 0)
 		return (0);
 	ret = 0;
 	if(*buff == 0)
 	{
 		read_bytes = read(fd, buff, BUFFER_SIZE);
-		printf("----------- in if(*buff == 0) -----------\n");
-		/*
-		 * 에러 발생?
-		 *
-		 * EOF 처리는 read() 에서 처리
-		 * return : 읽은 바이트 수
-		 * -1 : 에러
-		 * 0 : 파일의
-		 */
+		if (read_bytes < 1)	// -1 : 에러, 0 : EOF
+			return NULL;
 	}
-	while(add_line(ret, buff, read_bytes))
+	append_result = 0;
+	while (!is_contain_newline(buff) && read_bytes > 0)
 	{
-		printf("%s\n", ret);
-		if(*buff == 0)
-			return 0;
+		append_result = append_line(ret, buff);
+		if (append_result == NULL)
+			return NULL;
+		read_bytes = read(fd, buff, BUFFER_SIZE);
 	}
 
-	/*
-	 * 널을 찾을 떄 까지 읽는다.
-	 *
-	 */
+
+
 	printf("read_bytes: %zd\n",read_bytes);
 	printf("buff: %s\n", buff);
 	printf("--------------------\n");
